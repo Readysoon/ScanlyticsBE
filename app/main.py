a42b62cfb35de9f4eb5a1547df8795b59f725f07
Readysoon <p.gallaschik@gmail.com>
2024-08-01 22:34:23 +0200

    removed surreal test route

--- app/main.py
+++ app/main.py
@@ -7,68 +7,6 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
-
-async def surreal_db_operations():
-    async with Surreal("ws://surrealdb:8000/rpc") as db:
-    # use the following line to run uvicorn locally, the upper in docker
-    # async with Surreal("ws://localhost:8000/rpc") as db:
-    
-        await db.signin({"user": "root", "pass": "root"})
-        await db.use("test", "test")
-        logging.info("Connected to SurrealDB with namespace 'test' and database 'test'")
-
-        create_response = await db.create(
-            "person",
-            {
-                "user": "me",
-                "pass": "safe",
-                "marketing": True,
-                "tags": ["python", "documentation"],
-            },
-        )
-        logging.info(f"Create response: {create_response}")
-
-        persons = await db.select("person")
-        logging.info(f"Persons after creation: {persons}")
-
-        update_response = await db.update("person", {
-            "user": "you",
-            "pass": "very_safe",
-            "marketing": False,
-            "tags": ["Awesome"]
-        })
-        # delete_response = await db.delete("person")
-
-        query_response = await db.query("""
-        insert into person {
-            user: 'me',
-            pass: 'very_safe',
-            tags: ['python', 'documentation']
-        };
-        """)
-
-        select_response = await db.query("select * from person")
-        update_query_response = await db.query("""
-        update person content {
-            user: 'you',
-            pass: 'more_safe',
-            tags: ['awesome']
-        };
-        """)
-
-        select_response = await db.query("select * from person")
-        # delete_query_response = await db.query("delete person")
-
-        return {
-            "persons": persons,
-            "update_response": update_response,
-            # "delete_response": delete_response,
-            "query_response": query_response,
-            "select_response": select_response,
-            "update_query_response": update_query_response,
-            # "delete_query_response": delete_query_response,
-            "select_response": select_response,
-        }
     
 async def CreateUserService():
     async with Surreal("ws://surrealdb:8000/rpc") as db:
@@ -88,11 +26,6 @@ async def create_user():
     result = await CreateUserService()
     return result
 
-@app.get("/surrealdbOP")
-async def surrealdb_handler():
-    result = await surreal_db_operations()
-    return result
-
 @app.get("/surrealdb")
 async def surrealdb_handler():
     # Instead of returning the result, redirect to the desired URL
@@ -107,8 +40,6 @@ async def landing_page():
         </head>
         <body>
             <h1>Welcome to the FastAPI App</h1>
-            <p>To perform operations with SurrealDB, visit the following link:</p>
-            <a href="/surrealdbOP">Go to SurrealDB Operations</a>
             <p>To checkout the database itself, visit:</p>
             <a href="/surrealdb">Go to SurrealDB Operations</a>
             <p>To try creating the user table, visit:</p>
