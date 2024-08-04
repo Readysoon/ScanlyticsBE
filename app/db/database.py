6b57deecd7e7873a9cff2ed1a31d1eb8be28464e
Readysoon <p.gallaschik@gmail.com>
2024-08-05 00:21:54 +0200

    added doctor_report and polished the other tables with comments

--- app/db/database.py
+++ app/db/database.py
@@ -9,12 +9,16 @@ async def initializedb():
         logging.info("Connected to SurrealDB with namespace 'test' and database 'test'")
 
         create_response = await db.query(
+
+            # User table 
+            # -> users are doctors
             "DEFINE TABLE user schemafull;"
             "DEFINE FIELD mail on user TYPE string;"
             "DEFINE FIELD password on user TYPE string;"
             "DEFINE FIELD praxis ON user TYPE record(praxis);"
 
             # Practice table
+            # -> doctors collaborate in practices
             "DEFINE FIELD in ON TABLE works_at TYPE record<user>;"
             "DEFINE FIELD out ON TABLE works_at TYPE record<praxis>;"
 
@@ -22,6 +26,7 @@ async def initializedb():
             "DEFINE FIELD name ON praxis TYPE string;"
 
             # Patient table
+            # -> doctors have patients
             "DEFINE FIELD in ON TABLE cares_for TYPE record<user>;"
             "DEFINE FIELD out ON TABLE cares_for TYPE record<patient>;"
 
@@ -30,11 +35,14 @@ async def initializedb():
             "DEFINE FIELD email ON patient TYPE string;"
 
             # Doctor_Report table
+            # -> to enable the doctors to have their personal collection of templates with which they can replace ours 
+            # -> in contrast to radreport_report which are the 433 report templates we take from radreport (or somewhere else)
             "DEFINE FIELD in ON TABLE writes TYPE record<user>;"
             "DEFINE FIELD out ON TABLE writes TYPE record<doctor_report>;"
 
-            
-
+            "DEFINE TABLE doctor_report schemafull;"
+            "DEFINE FIELD name ON doctor_report TYPE string;"
+            "DEFINE FIELD text ON doctor_report TYPE string;"
 
         )
         
