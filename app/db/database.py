2e6e24ca5469cd1ab20efecc7237bfdc961616a5
Readysoon <p.gallaschik@gmail.com>
2024-08-05 00:08:51 +0200

    added relation between user and doctor_report

--- app/db/database.py
+++ app/db/database.py
@@ -9,16 +9,29 @@ async def initializedb():
         logging.info("Connected to SurrealDB with namespace 'test' and database 'test'")
 
         create_response = await db.query(
-            "DEFINE TABLE praxis schemafull;"
-            "DEFINE FIELD name on praxis type string;"
+            "DEFINE TABLE user schemafull;"
+            "DEFINE FIELD mail on user TYPE string;"
+            "DEFINE FIELD password on user TYPE string;"
+            "DEFINE FIELD praxis ON user TYPE record(praxis);"
 
+            # Practice table
             "DEFINE FIELD in ON TABLE works_at TYPE record<user>;"
             "DEFINE FIELD out ON TABLE works_at TYPE record<praxis>;"
-              
-            "DEFINE TABLE user schemafull;"
-            "DEFINE FIELD mail on user type string;"
-            "DEFINE FIELD password on user type string;"
-            "DEFINE FIELD praxis ON user type record(praxis)"
+
+            "DEFINE TABLE praxis schemafull;"
+            "DEFINE FIELD name ON praxis TYPE string;"
+
+            # Patient table
+            "DEFINE FIELD in ON TABLE cares_for TYPE record<user>;"
+            "DEFINE FIELD out ON TABLE cares_for TYPE record<patient>;"
+
+            "DEFINE TABLE patient schemafull;"
+            "DEFINE FIELD name ON patient TYPE string;"
+            "DEFINE FIELD email ON patient TYPE string;"
+
+            
+
+
         )
         
         logging.info(f"Create response: {create_response}")
