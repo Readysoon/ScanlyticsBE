a7bb781151e9b9fd37a49f875ca5d43850bd721c
Readysoon <p.gallaschik@gmail.com>
2024-08-06 19:33:01 +0200

    outsourcing environment variables; step 3: database.py

--- app/db/database.py
+++ app/db/database.py
@@ -3,10 +3,14 @@ import logging
 from surrealdb import Surreal
 
 DATABASE_URL = os.getenv("SURREALDB_URL", "ws://surrealdb:8000/rpc")
+DATABASE_USER = os.getenv("SURREALDB_USER", "root")
+DATABASE_PASS = os.getenv("SURREALDB_PASS", "root")
+DATABASE_NAMESPACE = os.getenv("SURREALDB_NAMESPACE", "test")
+DATABASE_NAME = os.getenv("SURREALDB_DATABASE", "test")
 
 async def get_db():
     logging.info(f"Attempting to connect to SurrealDB at {DATABASE_URL}")
-    
+
     try:
         db = Surreal(DATABASE_URL)
         logging.info("Created SurrealDB instance")
@@ -14,11 +18,11 @@ async def get_db():
         await db.connect()
         logging.info("Connected to SurrealDB")
         
-        await db.signin({"user": "root", "pass": "root"})
+        await db.signin({"user": DATABASE_USER, "pass": DATABASE_PASS})
         logging.info("Signed in to SurrealDB")
         
-        await db.use("test", "test")
-        logging.info("Using database 'test' and namespace 'test'")
+        await db.use(DATABASE_NAMESPACE, DATABASE_NAME)
+        logging.info(f"Using namespace '{DATABASE_NAMESPACE}' and database '{DATABASE_NAME}'")
         
         yield db
     except Exception as e:
@@ -27,4 +31,3 @@ async def get_db():
     finally:
         await db.close()
         logging.info("Closed SurrealDB connection")
-
