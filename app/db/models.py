8b7051253fce4457dc5f18f2d6a7eb9b26f473ae
Readysoon <p.gallaschik@gmail.com>
2024-08-06 18:47:10 +0200

    updated models.py accordingly, testing now...

--- app/db/models.py
+++ app/db/models.py
@@ -3,86 +3,84 @@ from surrealdb import Surreal
 import logging
 
 async def initializedb():
-    async with Surreal("ws://surrealdb:8000/rpc") as db:
-        await db.signin({"user": "root", "pass": "root"})
-        await db.use("test", "test")
-        logging.info("Connected to SurrealDB with namespace 'test' and database 'test'")
+    db = await get_db()
+    logging.info("Connected to SurrealDB with namespace 'test' and database 'test'")
 
-        create_response = await db.query(
-            # Practice table
-            # -> doctors collaborate in practices
-            "DEFINE FIELD in ON TABLE works_at TYPE record<user>;"
-            "DEFINE FIELD out ON TABLE works_at TYPE record<praxis>;"
+    create_response = await db.query(
+        # Practice table
+        # -> doctors collaborate in practices
+        "DEFINE FIELD in ON TABLE works_at TYPE record<user>;"
+        "DEFINE FIELD out ON TABLE works_at TYPE record<praxis>;"
 
-            "DEFINE TABLE praxis schemafull;"
-            "DEFINE FIELD name ON praxis TYPE string;"
+        "DEFINE TABLE praxis schemafull;"
+        "DEFINE FIELD name ON praxis TYPE string;"
 
 
-            # User table 
-            # -> users are doctors
-            "DEFINE TABLE user schemafull;"
-            "DEFINE FIELD mail on user TYPE string;"
-            "DEFINE FIELD password on user TYPE string;"
-            "DEFINE FIELD praxis ON user TYPE record(praxis);"
+        # User table 
+        # -> users are doctors
+        "DEFINE TABLE user schemafull;"
+        "DEFINE FIELD mail on user TYPE string;"
+        "DEFINE FIELD password on user TYPE string;"
+        "DEFINE FIELD praxis ON user TYPE record(praxis);"
 
 
-            # Note table
-            # -> doctors write notes for every patient
-            "DEFINE FIELD in ON TABLE takes TYPE record<user>;"
-            "DEFINE FIELD out ON TABLE takes TYPE record<note>;"
+        # Note table
+        # -> doctors write notes for every patient
+        "DEFINE FIELD in ON TABLE takes TYPE record<user>;"
+        "DEFINE FIELD out ON TABLE takes TYPE record<note>;"
 
-            "DEFINE TABLE note schemafull;"
-            "DEFINE FIELD text ON note TYPE string;"
+        "DEFINE TABLE note schemafull;"
+        "DEFINE FIELD text ON note TYPE string;"
 
-            "DEFINE FIELD in ON TABLE is_for TYPE record<note>;"
-            "DEFINE FIELD out ON TABLE is_for TYPE record<patient>;"
-
-
-            # Patient table
-            # -> doctors have patients
-            # -> is related to the radreport_reports which then links to the doctor_report table 
-            # so doctors can replace it with their own
-            "DEFINE FIELD in ON TABLE cares_for TYPE record<user>;"
-            "DEFINE FIELD out ON TABLE cares_for TYPE record<patient>;"
-
-            "DEFINE TABLE patient schemafull;"
-            "DEFINE FIELD name ON patient TYPE string;"
-            "DEFINE FIELD email ON patient TYPE string;"
+        "DEFINE FIELD in ON TABLE is_for TYPE record<note>;"
+        "DEFINE FIELD out ON TABLE is_for TYPE record<patient>;"
 
 
-            # Scan table
-            "DEFINE FIELD in ON TABLE was_scanned TYPE record<patient>;"
-            "DEFINE FIELD out ON TABLE was_scanned TYPE record<scan>;"
+        # Patient table
+        # -> doctors have patients
+        # -> is related to the radreport_reports which then links to the doctor_report table 
+        # so doctors can replace it with their own
+        "DEFINE FIELD in ON TABLE cares_for TYPE record<user>;"
+        "DEFINE FIELD out ON TABLE cares_for TYPE record<patient>;"
 
-            "DEFINE TABLE scan schemafull;"
-            "DEFINE FIELD name ON scan TYPE string;"
-            # this looks like this because i dont know better
-            "DEFINE FIELD image ON scan TYPE array;"
-
-            # a scan will be categorized by our ML model written by the wonderfull Julius K. to the correct radreport_report
-            "DEFINE FIELD in ON TABLE categorized_to TYPE record<scan>;"
-            "DEFINE FIELD out ON TABLE categorized_to TYPE record<radreport_report>;"
+        "DEFINE TABLE patient schemafull;"
+        "DEFINE FIELD name ON patient TYPE string;"
+        "DEFINE FIELD email ON patient TYPE string;"
 
 
-            # Doctor_Report table
-            # -> to enable the doctors to have their personal collection of templates with which they can replace ours 
-            # -> in contrast to radreport_report which are the 433 report templates we take from radreport (or somewhere else)
-            "DEFINE FIELD in ON TABLE writes TYPE record<user>;"
-            "DEFINE FIELD out ON TABLE writes TYPE record<doctor_report>;"
+        # Scan table
+        "DEFINE FIELD in ON TABLE was_scanned TYPE record<patient>;"
+        "DEFINE FIELD out ON TABLE was_scanned TYPE record<scan>;"
 
-            "DEFINE TABLE doctor_report schemafull;"
-            "DEFINE FIELD name ON doctor_report TYPE string;"
-            "DEFINE FIELD text ON doctor_report TYPE string;"
+        "DEFINE TABLE scan schemafull;"
+        "DEFINE FIELD name ON scan TYPE string;"
+        # this looks like this because i dont know better
+        "DEFINE FIELD image ON scan TYPE array;"
 
-            "DEFINE FIELD in ON TABLE replaces TYPE record<doctor_report>;"
-            "DEFINE FIELD out ON TABLE replaces TYPE record<radreport_report>;"
+        # a scan will be categorized by our ML model written by the wonderfull Julius K. to the correct radreport_report
+        "DEFINE FIELD in ON TABLE categorized_to TYPE record<scan>;"
+        "DEFINE FIELD out ON TABLE categorized_to TYPE record<radreport_report>;"
 
 
-            # Radreport_Report table
-            # -> in contrast this is our collection of reports, therefore not connected to the other tables (so far)
-            "DEFINE TABLE radreport_report schemafull;"
-            "DEFINE FIELD name ON radreport_report TYPE string;"
-            "DEFINE FIELD text ON radreport_report TYPE string;"
-        )
-        
-        logging.info(f"Create response: {create_response}")
+        # Doctor_Report table
+        # -> to enable the doctors to have their personal collection of templates with which they can replace ours 
+        # -> in contrast to radreport_report which are the 433 report templates we take from radreport (or somewhere else)
+        "DEFINE FIELD in ON TABLE writes TYPE record<user>;"
+        "DEFINE FIELD out ON TABLE writes TYPE record<doctor_report>;"
+
+        "DEFINE TABLE doctor_report schemafull;"
+        "DEFINE FIELD name ON doctor_report TYPE string;"
+        "DEFINE FIELD text ON doctor_report TYPE string;"
+
+        "DEFINE FIELD in ON TABLE replaces TYPE record<doctor_report>;"
+        "DEFINE FIELD out ON TABLE replaces TYPE record<radreport_report>;"
+
+
+        # Radreport_Report table
+        # -> in contrast this is our collection of reports, therefore not connected to the other tables (so far)
+        "DEFINE TABLE radreport_report schemafull;"
+        "DEFINE FIELD name ON radreport_report TYPE string;"
+        "DEFINE FIELD text ON radreport_report TYPE string;"
+    )
+    
+    logging.info(f"Create response: {create_response}")
