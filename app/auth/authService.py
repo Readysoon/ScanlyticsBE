058c0880ff3e3ce5977c13417f1dfa30038a105e
Readysoon <p.gallaschik@gmail.com>
2024-08-07 23:50:46 +0200

    added comment

--- app/auth/authService.py
+++ app/auth/authService.py
@@ -20,6 +20,7 @@ async def signup_service(username, password, db: Surreal = Depends(get_db)):
         if username == user_mail_in_db:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    # if theres no result return the exception:
     else: 
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Email not registered"
