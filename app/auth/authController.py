262a5fe6def281b232eae4a0241f2dac4303cd0e
Readysoon <p.gallaschik@gmail.com>
2024-08-10 20:23:35 +0200

    added check_mail_service and signup_service (so far)

--- app/auth/authController.py
+++ app/auth/authController.py
@@ -4,17 +4,23 @@ from surrealdb import Surreal
 
 from db.database import get_db
 
-from . import authSchema
-from . import authService
-
-from .authService import signup_service
-
+from .authService import check_mail_service, signup_service
 
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
 )
-# ''', response_model=authSchema.Token'''
+
+# first check if the mail is already in the database before entering all the other information
+@router.post("/check_mail")
+async def check_mail(
+    user_email: EmailStr = Form(...),
+    db: Surreal = Depends(get_db)
+    ):
+    return await check_mail_service(user_email, db)
+
+
+''', response_model=authSchema.Token'''
 @router.post("/orga_signup" )
 async def orga_signup(
     user_email: EmailStr = Form(...),
@@ -25,7 +31,7 @@ async def orga_signup(
     orga_address: str = Form(...),
     orga_email: EmailStr = Form(...),
     orga_name: str = Form(...),
-    
+
     db: Surreal = Depends(get_db)  # Use SurrealDB connection instead of SQLAlchemy session
     ):
     return await signup_service(user_email, user_name, user_password, user_role, orga_address, orga_email, orga_name, db)
--- app/auth/authService.py
+++ app/auth/authService.py
@@ -8,40 +8,64 @@ from db.database import get_db
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-async def signup_service(username, password, db: Surreal = Depends(get_db)):
-    # Select a specific record from a table
-    # In the secure surrealDB way: 
-    results = await db.query("SELECT * FROM user WHERE mail = $username", {"username": username})
-    # check if there are results:
-    if results and 'result' in results[0] and len(results[0]['result']) > 0:
-        # Extract the mail from the first record in the result set
-        # [
-        #   {
-        #       'result': [
-        #           {
-        #               'id': 'user:ctpgpv30btvkjxkvhd9q',
-        #               'mail': 'haha@gmail.com', 
-        #               'password': '1234',
-        #               'praxis': 'praxis:diagnostikum'
-        #            }
-        #        ],
-        #       'status': 'OK',
-        #       'time': '481.125µs'
-        #   }
-        # ]
-        user_mail_in_db = results[0]['result'][0]['mail']
-        # if it doesnt match then theres no user with this mail in the database
-        if not username == user_mail_in_db:
-            hashed_password =  pwd_context.hash(password)
-            print(username)
-            print(hashed_password)
-            raise HTTPException(
-                status_code=status.HTTP_201_CREATED, detail="Email not registered")
-    # if theres no result for this username/mail return the exception:
-        else: 
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT, detail="Email already registered"
-            )
-            
+async def check_mail_service(user_email, db):
+    try:
+        result = await db.query(f"SELECT VALUE email FROM User WHERE email = '{user_email}';")
+        email = result[0]['result']
+        print(email)
+
+        if email:
+            return HTTPException(status_code=status.HTTP_226_IM_USED, detail="Email in use")
+        else:
+            return HTTPException(status_code=status.HTTP_202_ACCEPTED, detail="Email can be registered")         
+         
+    except: 
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something querying the email didnt work")
+     
 
 
+async def signup_service(user_email, user_name, user_password, user_role, orga_address, orga_email, orga_name, db):
+    try: 
+        try:
+            create_orga_result = await db.query(f"CREATE Organization Set address = '{orga_address}', name = '{orga_name}', email = '{orga_email}';") 
+            create_orga_status = create_orga_result[0]['status']
+            create_orga_info = create_orga_result[0]['result']
+            if create_orga_status == "ERR":
+                raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR , detail=f"{create_orga_info}")
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Something creating the Organization didnt work: {e}")
+        
+        try:
+            select_orga_result = await db.query(f"SELECT VALUE id FROM Organization WHERE email = '{orga_email}';")
+            orga_id = select_orga_result[0]['result'][0]
+            print(orga_id)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Querying the just created Organization didnt work: {e}")
+        
+        hashed_password = pwd_context.hash(user_password)
+        try:
+            create_user_result = await db.query(f"CREATE User Set email = '{user_email}', name = '{user_name}', password = '{hashed_password}', role = '{user_role}', organization = '{orga_id}';")
+            create_user_status = create_user_result[0]['status']
+            create_user_info = create_user_result[0]['result']
+            if create_user_status == "ERR":
+                raise HTTPException(status_code=status.HTTP_226_IM_USED, detail=f"{create_user_info}")
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Something creating the User didnt work: {e}")
+        
+        
+        return HTTPException(status_code=status.HTTP_201_CREATED, detail = 
+                            f"Orga_address: '{orga_address}'"
+                            f"Orga_name: '{orga_name}'"
+                            f"Orga_email: '{orga_email}'"
+                            f"Orga_id: '{orga_id}'"
+                            f"User_email: '{user_email}'"
+                            f"User_name: '{user_name}'"
+                            f"Orga_role '{user_role}'"
+                            )
+
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Adding the user didnt work: {e}")
+
+
+
+        
