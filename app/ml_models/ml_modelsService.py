e290ad00fcd11fd418a93b64e698be15d4034d3c
julklingel <julklingel@gmail.com>
2024-10-23 21:55:47 +0200

    pydantic fixes

--- app/ml_models/ml_modelsService.py
+++ app/ml_models/ml_modelsService.py
@@ -4,37 +4,30 @@ from app.db.database import DatabaseResultService
 import boto3
 from botocore.exceptions import ClientError
 from fastapi.responses import JSONResponse
-from pydantic import BaseSettings
+import os
 from functools import lru_cache
 
-class S3Settings(BaseSettings):
-    S3_ENDPOINT_URL: str
-    S3_ACCESS_KEY: str
-    S3_SECRET_KEY: str
-    S3_BUCKET_NAME: str
-
-    class Config:
-        env_file = ".env"
-
 @lru_cache()
-def get_settings():
-    return S3Settings()
+def get_s3_client():
+    return boto3.client(
+        's3',
+        endpoint_url=os.getenv('S3_ENDPOINT_URL'),
+        aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
+        aws_secret_access_key=os.getenv('S3_SECRET_KEY')
+    )
 
-settings = get_settings()
-s3_client = boto3.client(
-    's3',
-    endpoint_url=settings.S3_ENDPOINT_URL,
-    aws_access_key_id=settings.S3_ACCESS_KEY,
-    aws_secret_access_key=settings.S3_SECRET_KEY
-)
+s3_client = get_s3_client()
 
 async def GetModel(model_name, current_user_id, db):
+    model_name_str = model_name.model_name if hasattr(model_name, 'model_name') else model_name
+    model_name_str = model_name_str.replace('.onnx', '')
+    
     try:
         url = s3_client.generate_presigned_url(
             'get_object',
             Params={
-                'Bucket': settings.S3_BUCKET_NAME,
-                'Key': f'{model_name}.onnx'
+                'Bucket': os.getenv('S3_BUCKET_NAME'),
+                'Key': f'{model_name_str}.onnx'
             },
             ExpiresIn=3600
         )
