b90ae7272d45e40f30400018cee2e9860cad15ce
Readysoon <p.gallaschik@gmail.com>
2024-10-23 13:22:53 +0200

    env variable fix -> setting them in fly.io

--- app/email/emailService.py
+++ app/email/emailService.py
@@ -9,7 +9,7 @@ from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 MAIL_FROM = os.getenv("MAIL_FROM")
-MAIL_PORT = int(os.getenv("MAIL_PORT"))  # Convert to integer
+MAIL_PORT = int(os.getenv("MAIL_PORT", 587))  # Convert to integer
 MAIL_SERVER = os.getenv("MAIL_SERVER")
 MAIL_FROM_NAME = os.getenv("MAIL_FROM_NAME")
 MAIL_STARTTLS = os.getenv("MAIL_STARTTLS") == "True"  # Convert to boolean
